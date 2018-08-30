f13fbd5336595aaf88b005c7998c47ad3dd9c0df
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2018-08-30 11:20:58 -0500

    Added Create-MrScriptBlock function

---
+++ MrModuleBuildTools/public/Create-MrScriptBlock.ps1
@@ -0,0 +1,34 @@
+﻿#Requires -Version 3.0
+function Create-MrScriptBlock {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [ValidateScript({
+            If (Test-Path -Path $_) {
+                $true
+            }
+            else {
+                Throw "'$_' is not valid."
+            }
+        })]
+        [Alias('FilePath', 'FileName')]
+        [string[]]$Path = ('.\*.ps1', '.\*.psm1')
+    )
+
+    PROCESS {
+        $Files = Get-ChildItem -Path $Path -Exclude *tests.ps1, *profile.ps1 |
+                 Select-Object -ExpandProperty FullName
+        foreach ($File in $Files) {
+            $Content = Get-Content -Path $File | Out-String
+            try {
+                [scriptblock]::Create($Content)
+            }
+            catch {
+                Write-Warning -Message 'An error occurred'
+            }
+               
+        }               
+    }
+}
+
