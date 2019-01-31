b71adc0b2005652b2c342aaf0d19248204727ed4
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2019-01-31 13:57:37 -0600

    Added unit test for Get-MrPrivateCommand

---
+++ MrModuleBuildTools/tests/Get-MrPrivateCommand.Tests.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [string]$Module = 'MrModuleBuildTools'
+)
+
+$CommandName = $MyInvocation.MyCommand.Name.Replace('.Tests.ps1', '')
+Write-Host -Object "Running $PSCommandPath" -ForegroundColor Cyan
+
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path) -replace 'Tests', 'Public'
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe 'Get-MrPrivateCommand' {
+    It "Doesn't return any public functions" {
+        Get-MrPrivateCommand -Module $Module |
+        ForEach-Object {
+            Get-Command -Name $_.Name -ErrorAction SilentlyContinue -Module $_.Source
+        } |
+        Should -BeNullOrEmpty
+    }
+}
