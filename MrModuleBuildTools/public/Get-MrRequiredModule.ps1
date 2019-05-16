6c8e72e51570d4cc3c7bf76ef8bf63eee8026807
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2019-05-16 12:17:37 -0500

    Added comment based help

--- MrModuleBuildTools/public/Get-MrRequiredModule.ps1
+++ MrModuleBuildTools/public/Get-MrRequiredModule.ps1
@@ -1,4 +1,37 @@
-﻿function Get-MrRequiredModule {
+﻿#Requires -Version 3.0
+function Get-MrRequiredModule {
+
+<#
+.SYNOPSIS
+    Gets a list of the required modules.
+ 
+.DESCRIPTION
+    Get-MrRequiredModule is an advanced function that returns a list of the required module dependencies from one or more
+    PS1 and/or PSM1 files.
+ 
+ .PARAMETER Path
+    Specifies a path to one or more locations. Wildcards are permitted. The default location is the current directory.
+
+.PARAMETER Code
+    The code to get the required modules for. If Get-Content is being used to obtain the code, use its -Raw parameter
+    otherwise the formating of the code will be lost.
+
+.PARAMETER Detailed
+    Return a detailed list of all of the modules including built-in modules that are required. This option does not
+    reply on a Requires statement.
+ 
+.EXAMPLE
+     Get-MrRequiredModule -Path 'C:\Scripts'
+
+.EXAMPLE
+     Get-MrRequiredModule -Code 'function Get-PowerShellProcess {Get-Process -Name PowerShell}' -Detailed
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>    
+
     [CmdletBinding(DefaultParameterSetName='File')]
     param(
         [Parameter(ValueFromPipeline,
@@ -27,7 +60,7 @@
              Select-Object -ExpandProperty RequiredModules -Unique).Name
         }
         else {
-            $PSBoundParameters.Remove('Detailed')
+            $PSBoundParameters.Remove('Detailed') | Out-Null
             $AllAST = Get-MrAst @PSBoundParameters
             
             foreach ($AST in $AllAST){
